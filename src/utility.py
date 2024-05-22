ee04dc42c233b512ce6f5b93478e8c7f24f06057
jeremyout <22989572+jeremyout@users.noreply.github.com>
2024-05-21 19:11:49 -0500

    Split delimiter

--- src/utility.py
+++ src/utility.py
@@ -26,3 +26,22 @@ def text_node_to_html_node(text_node):
     if text_node.text_type == text_type_image:
         return LeafNode(tag="img", value="",props={"src":text_node.url, "alt":text_node.text})
     raise ValueError(f"Invalid text type: {text_node.text_type}")
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type != text_type_text:
+            new_nodes.append(node)
+        split_nodes = []
+        sections = node.text.split(delimiter)
+        if len(sections) % 2 == 0:
+            raise ValueError("Invalid markdown, formatted section not closed")
+        for i,section in enumerate(sections):
+            if sections[i] == "":
+                continue
+            if i % 2 == 0:
+                split_nodes.append(TextNode(sections[i], text_type_text))
+            else:
+                split_nodes.append(TextNode(sections[i], text_type))
+        new_nodes.extend(split_nodes)
+    return new_nodes
